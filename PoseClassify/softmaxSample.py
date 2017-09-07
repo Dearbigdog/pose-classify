e517c069593fae3e63314bb714d0b905ed6cb76d
dearbigdog <13816466448@163.com>
2017-09-07 13:47:33 +0800

    init project

---
+++ PoseClassify/softmaxSample.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import  input_data
+mnist=input_data.read_data_sets("MNIST_data/",one_hot=True)
+print(mnist.test.images.shape)
+sess=tf.InteractiveSession()
+x=tf.placeholder(tf.float32,[None,784])
+W=tf.Variable(tf.zeros([784,10]))
+b=tf.Variable(tf.zeros([10]))
+y=tf.nn.softmax(tf.matmul(x,W)+b)
+y_=tf.placeholder(tf.float32,[None,10])
+cross_entropy=tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y),reduction_indices=[1]))
+train_step=tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+tf.global_variables_initializer().run()
+
+for i in range(1000):
+    batch_xs,batch_ys=mnist.train.next_batch(100)
+    train_step.run({x:batch_xs,y_:batch_ys})
+
+correct_predition=tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
+accuracy=tf.reduce_mean(tf.cast(correct_predition,tf.float32))
+print(accuracy.eval({x:mnist.test.images,y_:mnist.test.labels}))
